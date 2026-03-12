4ae2d87a3af671b8e4489269c4b26c17186d20fe
Ella <ella.bettison@xapien.com>
2026-03-12 13:10:52 +0000

    fix: Use single request for all SAE layers in get_features

--- run_gemma-3-27b.sh
+++ run_gemma-3-27b.sh
@@ -4,5 +4,5 @@ sudo make inference-localhost-install
 sudo make inference-localhost-build-gpu USE_LOCAL_HF_CACHE=1
 # CUDA
 sudo make inference-localhost-dev-gpu \
-     MODEL_SOURCESET=gemma-3-27b-it.gemmascope-2-res-65k \
+     MODEL_SOURCESET=gemma-3-27b-it.gemmascope-2-res-16k \
      USE_LOCAL_HF_CACHE=1
